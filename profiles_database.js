8c96295a4c9df9b4c210ca8bd8db080de77087c7
mrrmx <alexterranova81@gmail.com>
2020-05-25 15:34:08 -0400

    Add files via upload

---
+++ profiles_database.js
@@ -0,0 +1,6 @@
+   var profilesdb = [ 
+    { name: 'nikola tesla', dob: '7 10 1856' },
+    { name: 'john markoff', dob: '10 24 1949' },
+    { name: 'david icke', dob: '4 29 1952' },
+    { name: 'victor jules bergeron', dob: '12 10 1902' },
+]
