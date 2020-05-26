2f4d082a5a801c171d7bedcf6c297119d78991ed
mrrmx <alexterranova81@gmail.com>
2020-05-25 21:08:41 -0400

    Add files via upload

--- profiles_database.js
+++ profiles_database.js
@@ -1,6 +1,11 @@
    var profilesdb = [ 
     { name: 'nikola tesla', dob: '7 10 1856' },
     { name: 'john markoff', dob: '10 24 1949' },
-    { name: 'david icke', dob: '4 29 1952' },
+    { name: 'david vaughan icke', dob: '4 29 1952' },
     { name: 'victor jules bergeron', dob: '12 10 1902' },
+    { name: 'norman mortimer shine', dob: '10 8 1932' },
+    { name: 'kano jigoro', dob: '10 28 1860' },
+    { name: 'alfonso john romero', dob: '10 28 1967' },
+    { name: 'stephen william hawking', dob: '1 8 1942' },
+    { name: 'stephen robert irwin', dob: '2 22 1962' },
 ]
